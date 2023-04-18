ba678868fd4512224c7d46e506cefd5895dcc39d
nvv1d <blxxdrxvxn@navit.cloudns.ph>
2023-04-18 17:37:40 +0330

    Update start.sh

--- start.sh
+++ start.sh
@@ -69,8 +69,8 @@ run_web() {
     cp -f ./config.json /tmp/config.json
     sed -i "s|PASSWORD|${PASSWORD}|g;s|WSPATH|${WSPATH}|g" /tmp/config.json
     PATH_IN_LINK=$(echo ${WSPATH} | sed "s|\/|\%2F|g")
-    echo trojan://"${PASSWORD}@${PROJECT_DOMAIN}.glitch.me:443?security=tls&type=ws&path=${PATH_IN_LINK}#Glitch"
-    echo "Trojan Password: ${PASSWORD}, Websocket Path: ${WSPATH}, Domain: ${PROJECT_DOMAIN}.glitch.me, Port: 443"
+    echo trojan://"${PASSWORD}@${PROJECT_DOMAIN}.cyclic.app:443?security=tls&type=ws&path=${PATH_IN_LINK}#Cyclic"
+    echo "Trojan Password: ${PASSWORD}, Websocket Path: ${WSPATH}, Domain: ${PROJECT_DOMAIN}.cyclic.app, Port: 443"
     chmod +x ./web.js
     exec ./web.js -c /tmp/config.json >/dev/null 2>&1 &
 }
